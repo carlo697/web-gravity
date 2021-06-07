9153a76eb39cb6947f0a0a564aec716c1bd6660f
Carlos Peña <carlos.07888@gmail.com>
2021-06-06 22:23:09 -0400

    Add inputs to the sidebar for changing the render frames to skip and the square rendering option

--- js/App.js
+++ js/App.js
@@ -1,7 +1,9 @@
 import Game from "./Game.js";
+import { setupSidebar } from "./SettingsMenu.js";
 const canvas = document.getElementById("canvas");
 
 const game = new Game(canvas);
+setupSidebar();
 
 requestAnimationFrame(handleGameLoop);
 
--- js/SettingsMenu.js
+++ js/SettingsMenu.js
@@ -1,18 +1,30 @@
+import Game from "./Game.js";
+
 const sidebarButton = document.querySelector(".sidebar-button");
 const sidebarWrapper = document.querySelector(".sidebar-wrapper");
 const form = document.querySelector("form");
 
-setupEvents();
-
-function setupEvents() {
+export function setupSidebar() {
     sidebarButton.addEventListener("click", handleSidebarButtonClick);
     form.addEventListener("submit", handleSubmit);
+
+    applyChanges();
 }
 
 function handleSubmit(e) {
     e.preventDefault();
 
-    console.log("submit")
+    applyChanges();
+}
+
+function applyChanges() {
+    const formData = new FormData(form);
+
+    const skipRenderFrames = parseInt(formData.get("skipRenderFrames"));
+    const renderSquares = formData.get("renderSquares") === "on";
+    
+    Game.instance.skipRenderFrames = skipRenderFrames;
+    Game.instance.renderSquares = renderSquares;
 }
 
 function handleSidebarButtonClick() {
